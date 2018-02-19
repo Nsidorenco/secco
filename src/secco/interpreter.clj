367fdc4ca68cd2bc27cd7aaae0ce169c7543af72
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2018-02-19 13:40:36 +0100

    Add venv as mutable hashmap

--- src/secco/interpreter.clj
+++ src/secco/interpreter.clj
@@ -2,26 +2,34 @@
   [:require [instaparse.core :as insta] ]
   [:use  [secco.cfg]])
 
-(def venv
-  {})
+(def venv (atom {}))
+(swap! venv conj {:a 3})
+(println (get @venv :a))
 
 (def res (atom 0))
 
 (defn interpret-expression
   [exp]
   (insta/transform 
-    {:INT read-string,
-     :OPER read-string,
+    {:INT (fn [x] (reset! res (read-string x)))
+     :OPER read-string
+     :VarExp (fn [x] (reset! res (read-string x)))
      :OpExp (fn [exp1 oper exp2]
-              (if (= (str oper) "+")
-                (reset! res (+ exp1 exp2))
-                (println oper "+")))}
+              (cond (= (str oper) "+") (reset! res (+ exp1 exp2))
+                    (= (str oper) "<") (reset! res (< exp1 exp2))
+                    :else (println "oper not supported")))
+     :AssignExp (fn [varexp body]
+                  (interpret-expression varexp)
+                  (let [varname @res]
+                    (interpret-expression body)
+                    (swap! venv conj (varname @res))))}
     exp))
 
 (defn interpret [node]
   (if (instance? secco.cfg.CFGNode node)
     (let [guard (.exp node)]
-      (if (interpret-expression guard)
+      (interpret-expression guard)
+      (if @res
         (interpret (get-t node))
         (interpret (get-f node))))
     (println @res)))
@@ -29,6 +37,8 @@
 (interpret-expression [:OpExp [:INT "4"] [:OPER "+"] [:INT "4"]])
 (interpret-expression [:OPER "+"])
 
+(interpret (get-t(build "x := 4")))
+(interpret (build "if 3 < 2 then 4 else 5"))
 (interpret-expression (.exp (get-t(build "4+4"))))
 (interpret (->Node "oper" (.exp (get-t(build "4+4"))) nil nil))
 (interpret (build "4+4"))
