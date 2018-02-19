d8591fbb87eb83382e58947d03fd0b41615bcfd5
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2018-02-19 14:53:26 +0100

    Cleanup

--- src/secco/interpreter.clj
+++ src/secco/interpreter.clj
@@ -7,23 +7,6 @@
 
 (def res (atom 0))
 
-(defn interpret-expression_old
-  [exp]
-  (insta/transform 
-    {:INT (fn [x] (reset! res (read-string x)))
-     :OPER read-string
-     :VarExp (fn [x] (reset! res (read-string x)))
-     :OpExp (fn [exp1 oper exp2]
-              (cond (= (str oper) "+") (reset! res (+ exp1 exp2))
-                    (= (str oper) "<") (reset! res (< exp1 exp2))
-                    :else (println "oper not supported")))
-     :AssignExp (fn [varexp body]
-                  (interpret-expression varexp)
-                  (let [varname @res]
-                    (interpret-expression body)
-                    (swap! venv conj (varname @res))))}
-    exp))
-
 (defn interpret-expression
   [exp]
   (match [(first exp)]
@@ -57,7 +40,8 @@
 (interpret-expression [:OpExp [:INT "4"] [:OPER "+"] [:INT "4"]])
 (interpret-expression [:OPER "+"])
 
-(interpret (build "(x := 4; x := 7)"))
+(interpret (build "(x := 4; x := 7; x := 1021212131)"))
+(println @venv)
 (interpret (build "if 3 < 2 then 4 else 5"))
 (interpret-expression (.exp (get-t(build "4+4"))))
 (interpret (->Node "oper" (.exp (get-t(build "4+4"))) nil nil))
