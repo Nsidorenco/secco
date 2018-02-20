fcc2774b2d61c73f17ca9b36ed41866d58ab57bb
Jesper Brodersen <jesper.brodersen2@post.au.dk>
2018-02-20 17:26:49 +0100

    added z3 jar to deps, remember to mvn it locally

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject secco "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Secco - Minimal language for Symbolic Execution testing"
   :url "https://github.com/Nsidorenco/secco"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"],
                  [instaparse "1.4.8"],
-                 [org.clojure/core.match "0.3.0-alpha5"]]
+                 [org.clojure/core.match "0.3.0-alpha5"]
+                 [com.microsoft/z3 "4.5.1"]]
   :profiles {:dev {:dependencies
                    [[rhizome "0.2.9"]]}}
   :scm {:name "git"
