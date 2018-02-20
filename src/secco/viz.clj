72396b6b23792b8585e9c81a5a351427a247168a
Jesper Brodersen <jesper.brodersen2@post.au.dk>
2018-02-20 17:12:54 +0100

    prettify some assignexp

--- src/secco/viz.clj
+++ src/secco/viz.clj
@@ -33,6 +33,14 @@
 
 (def graph (atom {}))
 
+(defn pretty [body] 
+  (match [(first body)]
+         [:VarExp] (second body)
+         [:INT] (second body)
+         [:OpExp] (let [[_ exp1 oper exp2] body] 
+                    (pretty exp1) (pretty oper) (pretty exp2))
+         [_] body))
+
 (defn build-tree 
   ([node] (build-tree node 0 {}))
   ([node counter mark]
@@ -66,14 +74,14 @@
                          (swap! graph conj {getsym [(build-tree (cfg/get-t node) (+ counter 1) mark)]}) 
                          getsym)
               ["varexp"] (let [
-                               ex (.exp node)
+                               ex (second (.exp node))
                                ]
                            (swap! labels conj {getsym ex})
                            (swap! graph conj {getsym [(build-tree (cfg/get-t node) (+ counter 1) mark)]})
                            getsym)
               ["assignexp"] (let [
                                   [_ varexp body] (.exp node)
-                                  ex (str (second varexp) ":=" body)
+                                  ex (str (second varexp) ":=" (pretty body))
                                   og (.exp node)
                                   ]
                               (swap! labels conj {getsym ex})
