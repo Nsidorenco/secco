2ed6c6670ceafbf985d3cda224867f2465e6b7f1
Jesper Brodersen <jesper.brodersen2@post.au.dk>
2018-02-21 21:15:33 +0100

    some more ideas for result handler in sym-exp

--- src/secco/symbolic.clj
+++ src/secco/symbolic.clj
@@ -6,14 +6,14 @@
 
 ; define mutable variables
 (def venv (atom {}))
-(def res (atom 0))
+(def res (atom []))
 
 
 (defmacro set-res
   ([value]
   `(reset! res ~value))
   ([exp1 oper exp2]
-  `(set-res ((resolve ~oper) ~exp1 ~exp2))))
+  `(set-res (z3/assert exp1 oper exp2))))
 
 
 (defn sym-exp
@@ -24,26 +24,30 @@
          [:VarExp] (let [varname (get @venv (second exp))]
                      (assert (not= @res nil) "Variable not declared"))  
          [:OpExp] (let [[_ exp1 oper exp2] exp]
-                        ((sym-exp exp1) (sym-exp oper) (sym-exp exp1)))
+                    (sym-exp exp1)
+                    (let [exp1 @res]
+                      (sym-exp exp2)
+                      (let [exp2 @res oper (sym-exp oper)]
+                        (set-res exp1 oper exp2))))
          [:AssignExp] (let [[_ varexp bodyexp] exp
                             varname (sym-exp (second varexp))
                             body (sym-exp bodyexp)]
-                        (z3/const varname Int))
+                        (set-res (z3/const varname Int)))
          [_] @res))
 
 (defn model 
   ([node] (model node []))
   ([node build]
-  (if (instance? secco.cfg.CFGNode node)
-    (let [
-          tr (sym-exp (.exp (cfg/get-t node)))
-          fa (sym-exp (.exp (cfg/get-f node)))
-          ]
-      (model (cfg/get-t node) (conj build tr))
-      (model (cfg/get-f node) (conj build fa))))
-  build))
+   (if (instance? secco.cfg.CFGNode node)
+     (let [
+           tr (sym-exp (.exp (cfg/get-t node)))
+           fa (sym-exp (.exp (cfg/get-f node)))
+           ]
+       (model (cfg/get-t node) (conj build tr))
+       (model (cfg/get-f node) (conj build fa))))
+   build))
 
-(z3/check-sat (model (build "(x:=0;y:=8;while x<y do x:=x+1;y:=5)")))
+(z3/check-sat (model (cfg/build "(x:=0;y:=8;while x<y do x:=x+1;y:=5)")))
 
 (def whilemodel (conj [] 
                       (z3/const x Int)
