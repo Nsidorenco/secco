68b6f23261dd73e35f9135b03c7bd45180af8864
broeman <brodersendev@gmail.com>
2018-02-22 17:00:18 +0100

    some symbolic buildup, problems with not

--- src/secco/symbolic.clj
+++ src/secco/symbolic.clj
@@ -2,52 +2,63 @@
   [:require [instaparse.core :as insta]
             [secco.cfg :as cfg]
             [secco.z3 :as z3]
+            [secco.util :as util]
             [clojure.core.match :refer [match]]])
 
 ; define mutable variables
 (def venv (atom {}))
-(def res (atom []))
-
-
-(defmacro set-res
-  ([value]
-  `(reset! res ~value))
-  ([exp1 oper exp2]
-  `(set-res (z3/assert exp1 oper exp2))))
-
+(def sym (atom (into [] util/literals)))
 
 (defn sym-exp
   [exp]
+  (let [getsym (util/pop-head! sym)]
   (match [(first exp)]
          [:INT] (read-string (second exp)) 
          [:OPER] (read-string (second exp))
          [:VarExp] (let [varname (get @venv (second exp))]
-                     (assert (not= @res nil) "Variable not declared"))  
-         [:OpExp] (let [[_ exp1 oper exp2] exp]
-                    (sym-exp exp1)
-                    (let [exp1 @res]
-                      (sym-exp exp2)
-                      (let [exp2 @res oper (sym-exp oper)]
-                        (set-res exp1 oper exp2))))
+                     (assert (not= varname nil) "Variable not declared")
+                     varname)  
+         [:OpExp] (let [[_ exp1 oper exp2] exp
+                         e1 (sym-exp exp1)
+                         op (sym-exp oper)
+                         e2 (sym-exp exp2)]
+                    (z3/assert e1 op e2))
          [:AssignExp] (let [[_ varexp bodyexp] exp
-                            varname (sym-exp (second varexp))
+                            varname (second varexp)
                             body (sym-exp bodyexp)]
-                        (set-res (z3/const varname Int)))
-         [_] @res))
+                        (swap! venv conj {varname getsym})
+                        (z3/const (get @venv varname) Int))
+         [_] "")))
 
+(z3/not (assert (< 1 9)))
 (defn model 
   ([node] (model node []))
-  ([node build]
+  ([node pc] 
    (if (instance? secco.cfg.CFGNode node)
-     (let [
-           tr (sym-exp (.exp (cfg/get-t node)))
-           fa (sym-exp (.exp (cfg/get-f node)))
-           ]
-       (model (cfg/get-t node) (conj build tr))
-       (model (cfg/get-f node) (conj build fa))))
-   build))
+     (let [last_cond (sym-exp (.exp node))]
+       (if (instance? clojure.lang.PersistentList last_cond)
+         (let [t_pc (conj pc last_cond)
+               f_pc (conj pc last_cond)]
+           (println (z3/not last_cond))
+           (when (z3/check-sat t_pc) 
+             (model (cfg/get-t node) t_pc))
+           (when (z3/check-sat f_pc) 
+             (model (cfg/get-f node) f_pc))
+           (when-not (or (z3/check-sat t_pc) (z3/check-sat f_pc))
+             (println "pc: " pc)
+             pc))
+         (do 
+           (model (cfg/get-t node) pc))))
+     (println "pc: "pc))))
 
-(z3/check-sat (model (cfg/build "(x:=0;y:=8;while x<y do x:=x+1;y:=5)")))
+;(z3/check-sat (conj [] (z3/const x Int)))
+(reset! venv {})
+(println @venv)
+(get @venv "x")
+(model (cfg/build "(x:=0;if x < 0 then 0 else x)")) 
+(model (cfg/build "if 1 < 2 then 3 else 41"))
+
+;z3/check-sat (model (cfg/build "(x:=0;y:=8;while x<y do x:=x+1;y:=5)")))
 
 (def whilemodel (conj [] 
                       (z3/const x Int)
--- src/secco/z3.clj
+++ src/secco/z3.clj
@@ -2,29 +2,37 @@
   [:require [clojure.string :as s] ]
   [:use [clojure.java.shell :only [sh]]])
 
+(def sat?  
+  (list 'check-sat))
 
 (defn check-sat
   [model]
+  ;(println (str (s/join (conj model sat?))))
   (-> (sh "z3" "-in" :in (str (s/join (conj model sat?))))
       :out
       s/trim
       (#(if (= % "sat")
-          :sat
-          :unsat))))
+          true 
+          false))))
 
 (defmacro const
   "Coverts s-exp `(const name type)` to SMT format"
   [cname ctype]
-  `(list ~'(symbol 'declare-const) '~cname '~ctype))
+  `(list ~'(symbol 'declare-const) ~cname '~ctype))
 
 (defmacro assert
   "Converts s-exp `(assert var oper value)` to SMT format"
   [exp1 oper exp2]
-  `(list ~'(symbol 'assert) (list '~oper '~exp1 '~exp2)))
+  `(list ~'(symbol 'assert) (list ~oper ~exp1 ~exp2)))
 
-(def sat?  
-  (list 'check-sat))
+(defmacro not
+  "Not"
+  [exp]
+    `(list (symbol (first ~exp)) (list ~'(symbol 'not) (second ~exp))))
 
+(def test_exp (assert 1 < 9))
+(def k (first test_exp))
+(not test_exp)
 
 ; (def test_model (conj [] (list 'declare-const 'a 'Int) 
 ;                          (list 'assert (list '> 'a '10))))
@@ -32,3 +40,4 @@
 ; (def test_model2 (conj [] (const b Int) (assert b > 10) (assert b < 5)))
 
 ; (check-sat test_model2)
+;(sh "z3" "-in" :in (str (s/join (conj [] (list 'declare-const "x" 'Int) sat?))))
