fd61612d67d441e0b764f9d06c22c3564935b85b
broeman <brodersendev@gmail.com>
2018-02-22 14:55:33 +0100

    added pop-head on atom lists

--- src/secco/util.clj
+++ src/secco/util.clj
@@ -14,3 +14,10 @@
                   (subsets n (rest items)))))
 
 (def symbols (map (fn [x] (keyword (clojure.string/join x))) (subsets 3 alphabet)))
+(def literals (map (fn [x] (read-string (clojure.string/join x))) (subsets 3 alphabet)))
+
+(defn pop-head!
+  [a]
+  (-> (swap! a #(with-meta (subvec % 1) {:head (% 0)}))
+      (meta)
+      :head))
