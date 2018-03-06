15820f13d36b689cbde28bb63d338f814a80a39d
broeman <brodersendev@gmail.com>
2018-03-06 16:18:33 +0100

    Code cleanup and added missing arithmetic operations

--- src/secco/symbolic.clj
+++ src/secco/symbolic.clj
@@ -30,6 +30,10 @@
                  (z3/assert e1 op e2))
       [:add] (let [[_ exp1 exp2] exp]
                (arithmetic + (sym-exp exp1) (sym-exp exp2)))
+      [:sub] (let [[_ exp1 exp2] exp]
+               (arithmetic - (sym-exp exp1) (sym-exp exp2)))
+      [:mul] (let [[_ exp1 exp2] exp]
+               (arithmetic * (sym-exp exp1) (sym-exp exp2)))
       [:AssignExp] (let [[_ varexp bodyexp] exp
                          varname (second varexp)
                          body (sym-exp bodyexp)]
@@ -41,19 +45,18 @@
 (defn model
   ([node] (model node []))
   ([node pc]
-   (if (instance? secco.cfg.CFGNode node)
-     (let [last_cond (sym-exp (.exp node))]
-       (if (instance? clojure.lang.PersistentList last_cond)
-         (let [t_pc (conj pc last_cond)
-               f_pc (conj pc (z3/not last_cond))]
-           (when (z3/check-sat t_pc)
+   (when (z3/check-sat pc)
+     (if (instance? secco.cfg.CFGNode node)
+       (let [last_cond (sym-exp (.exp node))]
+         (if (instance? clojure.lang.PersistentList last_cond)
+           (let [t_pc (conj pc last_cond)
+                 f_pc (conj pc (z3/not last_cond))]
+             (model (cfg/get-f node) f_pc)
              (model (cfg/get-t node) t_pc))
-           (when (z3/check-sat f_pc)
-             (model (cfg/get-f node) f_pc)))
-         (model (cfg/get-t node) pc))
-       (when (isa? ::Error last_cond)
-         (println "Reached error state with pc: " pc)))
-     (println "pc: " pc))))
+           (if (isa? ::Error last_cond)
+             (println "Reached error state with pc: " pc "and venv:" @venv)
+             (model (cfg/get-t node) pc))))
+       (println "pc: " pc)))))
 
 ; (model (cfg/build "(a := 1 + 1; if a > 1 then 2 else 3)"))
 ; (z3/check-sat (conj [] (z3/const x Int)))
@@ -65,3 +68,4 @@
 ;(z3/check-sat (model (cfg/build "(x:=0;y:=8;while x<y do x:=x+1;y:=5)")))
 ; (model (cfg/build "(x:=input();y:=input(); if x<y then x:=x+y else y:=x)"))
 ; (model (cfg/build "(x:=input(); if x > 10 then error() else 1)"))
+;(model (cfg/build (slurp (clojure.java.io/resource "test-programs/gcd.sec"))))
