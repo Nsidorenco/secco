044b6521c6de90b10e87bf962da4ef35b571a65e
Nikolaj Sidorenco <Nikolaj176@hotmail.com>
2018-04-07 13:28:03 +0200

    Upgrade project to clojure 1.9

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/Nsidorenco/secco"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"],
+  :dependencies [[org.clojure/clojure "1.9.0"],
                  [instaparse "1.4.8"],
                  [org.clojure/core.match "0.3.0-alpha5"]]
                  
